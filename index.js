bf500727683696e277438daf234c26bde530be83
Binita Gupta <binitagupta24@gmail.com>
2022-09-07 22:14:08 -0400

    Add option to edit existing tasks

--- index.js
+++ index.js
@@ -18,8 +18,10 @@ mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => {
 
 app.set("view engine", "ejs");
 
-app.get('/',(req, res) => {
-    res.render('todo.ejs');
+app.get("/", (req, res) => {
+    TodoTask.find({}, (err, tasks) => {
+        res.render("todo.ejs", { todoTasks: tasks });
+    });
 });
 
 //POST METHOD
@@ -27,14 +29,26 @@ app.post('/',async (req, res) => {
     const todoTask = new TodoTask({
         content: req.body.content
     });
-
-    console.log('todotask', todoTask);
-
     try {
         await todoTask.save();
         res.redirect("/");
     } catch (err) {
-        console.log(err);
         res.redirect("/");
     }
+});
+
+// UPDATE
+app.route("/edit/:id")
+    .get((req, res) => {
+        const id = req.params.id;
+        TodoTask.find({}, (err, tasks) => {
+            res.render("todoEdit.ejs", { todoTasks: tasks, idTask: id });
+        });
+    })
+    .post((req, res) => {
+        const id = req.params.id;
+        TodoTask.findByIdAndUpdate(id, { content: req.body.content }, err => {
+        if (err) return res.send(500, err);
+        res.redirect("/");
+    });
 });
