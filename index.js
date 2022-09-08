ce0a5aa88e99de119a96356f1cc90ebb70831c4a
Binita Gupta <binitagupta24@gmail.com>
2022-09-07 20:43:10 -0400

    Configure database connection to MongoDB

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
 
 app.use("/static", express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+
+// Connection to MongoDB
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => {
+console.log("Connected to db!");
+    app.listen(3000, () => console.log("Server Up and running"));
+});
 
 app.set("view engine", "ejs");
 
@@ -9,5 +21,6 @@ app.get('/',(req, res) => {
     res.render('todo.ejs');
 });
 
-app.listen(3000, () => console.log("Server Up and running"));
-
+app.post('/',(req, res) => {
+    console.log(req.body);
+});
